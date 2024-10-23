43c16d4605777c880c18af60480e2fd70d4ae72d
vinothsakthikumar <vinod.s@mobizent.com>
2024-10-23 20:57:14 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/welcome/page.tsx 	modified:   src/app/welcome/styles.css

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -14,6 +14,20 @@ const Welcome = () => {
       <div className='container'>
         <div className='content'>
         <h1>Hello</h1>
+        <li className="item list">
+              <h3 className="title">Desigan</h3>
+              <p className="description">
+                            I'm a Java full-stack developer preparing for a Google job application,
+                            focusing on backend development with Spring Boot and 
+                            frontend development using React and Next.js.
+                            I'm also working on various projects involving REST APIs, databases, and are 
+                            interested in learning and improving my skills across different technologies.</p>
+              <h3 className="name">Java FullStack Developer</h3>
+              
+                <a href="https://github.com/Desigan-v?tab=repositories" className="view-link">
+                <i className="fa-brands fa-github"></i> GitHub <i className="fa-solid fa-square-up-right"></i>
+                </a>
+            </li>
         <button
         className='button-container'
         type='submit'
